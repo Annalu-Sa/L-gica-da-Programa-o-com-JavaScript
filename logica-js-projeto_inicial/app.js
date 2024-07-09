4f208a82095e05a8df7c59d37ec6d7a10118aa40
Anna Luisa Sá <91437093+Annalu-Sa@users.noreply.github.com>
2024-07-09 20:01:48 -0300

    Add files via upload

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,3 +1,25 @@
 alert('Boas vindas ao jogo do número secreto!');
-let secretNum = 2;
-prompt('Escolha um valor entre 1 a 30:');
+let numMaximo = 5000;
+let secretNum = parseint(Math.random() * numMaximo + 1);
+console.log(secretNum);
+let inputUser;
+let count = 1;
+
+while(inputUser != secretNum){
+    inputUser = prompt(`Escolha um valor entre 1 a ${numMaximo}:`);
+    if(secretNum == inputUser ) {
+        break;
+    }else {
+
+        if (secretNum > inputUser)
+            {
+                alert(`O número secreto é maior que ${inputUser}`);
+            }else {
+                alert(`O número secreto é menor que ${inputUser}`);
+            }
+            count++;
+    }
+}
+
+let palavraTentativa = count > 1 ? 'tentativas ' : 'tentativa';
+alert(`Parabéns! Você descobriu o número secreto ${inputUser} com ${count} ${palavraTentativa}`);
